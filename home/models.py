34eea2a2c858f4339c4708fd73f4217731e97628
Chris Rogers <chris.rogers@torchbox.com>
2015-06-25 13:09:15 +0100

    styling for homepage

--- home/models.py
+++ home/models.py
@@ -10,21 +10,20 @@ from blog.models import BlogPage
 class HomePage(Page):
     body = RichTextField(blank=True)
 
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
+
     def serve(self, request):
         # Get blogs
         blogs = BlogPage.objects.filter(live=True)
         blogs = blogs.order_by('-date')
         # Filter by tag
-        tag = request.GET.get('tag')
-        if tag:
-            blogs = blogs.filter(tags__name=tag)
+        # tag = request.GET.get('tag')
+        # if tag:
+        #     blogs = blogs.filter(tags__name=tag)
 
         return render(request, self.template, {
             'self': self,
             'blogs': blogs,
-     	})
-
-
-    content_panels = Page.content_panels + [
-        FieldPanel('body')
-    ]
+        })
