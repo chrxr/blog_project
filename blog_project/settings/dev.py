a479eab08b8a4b54718e58d5de5326188e727b86
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 22:12:18 +0000

    changing stuff

--- blog_project/settings/dev.py
+++ blog_project/settings/dev.py
@@ -2,8 +2,8 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
@@ -18,7 +18,7 @@ LOGGING = {
     	'file': {
     		'level': 'DEBUG',
     		'class': 'logging.FileHandler',
-    		'filename': SITE_ROOT + '/debug.log',
+    		'filename': BASE_DIR + '/debug.log',
     	},
     },
     'loggers': {
