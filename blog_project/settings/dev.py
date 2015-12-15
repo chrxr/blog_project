64978a1ffdbb98dda29c81816c4caac891cba947
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 21:48:57 +0000

    broke it

--- blog_project/settings/dev.py
+++ blog_project/settings/dev.py
@@ -8,6 +8,9 @@ TEMPLATE_DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
@@ -27,9 +30,21 @@ SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
 #     },
 # }
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
 
 try:
     from .local import *
