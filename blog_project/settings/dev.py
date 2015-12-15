f4c8538ab528677ff494f535da9f6b652ecace54
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 22:09:33 +0000

    changing stuff

--- blog_project/settings/dev.py
+++ blog_project/settings/dev.py
@@ -2,8 +2,8 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
