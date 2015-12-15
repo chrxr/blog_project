3f7b2745079e524708570ee32f3a3cc777293290
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 21:48:06 +0000

    broke it

--- blog_project/settings/dev.py
+++ blog_project/settings/dev.py
@@ -8,24 +8,24 @@ TEMPLATE_DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-    	'file': {
-    		'level': 'DEBUG',
-    		'class': 'logging.FileHandler',
-    		'filename': '~/blog_project/debug.log',
-    	},
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#     	'file': {
+#     		'level': 'DEBUG',
+#     		'class': 'logging.FileHandler',
+#     		'filename': '~/blog_project/debug.log',
+#     	},
+#     },
+#     'loggers': {
+#         'django.request': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -4,22 +4,6 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#             'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
-#         },
-#     },
-# }
-
 try:
     from .local import *
 except ImportError:
