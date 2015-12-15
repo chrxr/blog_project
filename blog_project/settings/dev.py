2e216776ebcf4009f795e6743741433cef278fd2
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 21:51:26 +0000

    broke it

--- blog_project/settings/dev.py
+++ blog_project/settings/dev.py
@@ -11,40 +11,40 @@ SECRET_KEY = 'mw)zkel7_@3@$=%f&*9b=g@hg9!&ur@+#)hs7m=$+s08a@=aya'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#     	'file': {
-#     		'level': 'DEBUG',
-#     		'class': 'logging.FileHandler',
-#     		'filename': '~/blog_project/debug.log',
-#     	},
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
+    	'file': {
+    		'level': 'DEBUG',
+    		'class': 'logging.FileHandler',
+    		'filename': '~/blog_project/debug.log',
+    	},
     },
     'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
         },
     },
 }
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console'],
+#             'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+#         },
+#     },
+# }
 
 try:
     from .local import *
