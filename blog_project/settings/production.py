4810d46f35738cdee87b3aa030df2f827ae28aad
Chris Rogers <chrxr@hotmail.co.uk>
2015-12-15 21:40:52 +0000

    logging

--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -4,6 +4,21 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 try:
     from .local import *
