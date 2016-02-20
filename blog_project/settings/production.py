2c58f9840d507c24fcf4d3ede45b56847dab7c35
Chris Rogers <chrxr@hotmail.co.uk>
2016-02-20 11:47:31 +0000

    changing production settings

--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -4,6 +4,31 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+env = os.environ.copy()
+
+SECRET_KEY = env['SECRET_KEY']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+    	'file': {
+    		'level': 'DEBUG',
+    		'class': 'logging.FileHandler',
+    		'filename': BASE_DIR + '/debug.log',
+    	},
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 try:
     from .local import *
 except ImportError:
