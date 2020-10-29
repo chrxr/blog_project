bd6312ed82580dc788c4cf4768484bf94ec30a7a
Chris Rogers <chrxr@outlook.com>
2020-10-29 10:47:03 -0400

    got the RDS working

--- blog_project/settings/base.py
+++ blog_project/settings/base.py
@@ -137,17 +137,21 @@ USE_S3 = os.getenv('USE_S3') == 'TRUE'
 print(USE_S3)
 
 if USE_S3:
+  # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+  # AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
   AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
   AWS_DEFAULT_ACL = 'public-read'
   AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
   AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
   # s3 static settings
   AWS_LOCATION = 'static'
-  PUBLIC_MEDIA_LOCATION = 'media'
+  PUBLIC_MEDIA_LOCATION = 'media' 
   DEFAULT_FILE_STORAGE = 'blog.storage_backends.PublicMediaStorage'
   MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
   STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
   STATICFILES_STORAGE = 'blog.storage_backends.StaticStorage'
+  COMPRESS_URL = STATIC_URL
+  COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
 else:
   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
   STATIC_URL = '/static/'
