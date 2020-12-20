bb978d1ea34ff085ff2a669260cada4a72ac56b8
Chris Rogers <chrxr@outlook.com>
2020-12-20 15:11:24 -0500

    sync file done and working on static cloud formation

--- s3sync.py
+++ s3sync.py
@@ -1,7 +1,35 @@
 import boto3
+from pathlib import Path
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+def defineContentType(suffix):
+  contentType = ''
+  if suffix == ".js":
+    contentType = "application/javascript"
+  elif suffix == ".css":
+    contentType = "text/css"
+  elif suffix == ".html":
+    contentType = "text/html"
+  return contentType
+
+
+s3 = boto3.client('s3')
+staticFolder = Path("staticsite")
+pathList = [['staticsite/index.html', 'index.html', 'text/html']]
+requiredFolders = ["static/css", "static/images", "static/js"]
+
+for x in staticFolder.iterdir():
+  if x.is_dir():
+    if x.name == "static":
+      for folder in requiredFolders:
+        subFolder = staticFolder / folder
+        for y in subFolder.iterdir():
+          contentType = defineContentType(y.suffix)
+          pathList.append([y.as_posix(), "{}/{}".format(folder, y.name), contentType])
+    else:
+      for y in x.iterdir():
+          contentType = defineContentType(y.suffix)
+          pathList.append([y.as_posix(), "{}/{}".format(x.name, y.name), contentType])
+
+for path in pathList:
+  s3.upload_file(path[0], "chrxr-static-website-bucket", path[1], ExtraArgs={'ContentType': path[2]})
